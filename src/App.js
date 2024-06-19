ebae38711c1222a839ca631c5a8c201ad1850cf4
nymishkash <reachnymish@gmail.com>
2024-06-19 10:03:30 +0530

    19th June 2024 (2)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Products from './Products';
-import { a, b } from './Products';
 
 function App() {
   return (
     <div className="App">
       <Products />
-      <a />
     </div>
   );
 }
